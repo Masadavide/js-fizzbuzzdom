8738026d9c2bd2ad166dc72034b4191977d161af
Masadavide <davide.masadm@gmail.com>
2021-12-10 15:43:10 +0100

    crea delle scatole da 1 a 100 nella pagina HTML

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+const scatola = document.getElementById('container')
+
+//scrive da 1 a 100 in console.log e in "scatola (HTML)"
+
 for(let i = 1; i <= 100; i++){
     console.log('iterazione: ', i);
+    scatola.innerHTML += '<div class="box">' + i + '</div>'
 }
+
+//milestone 1
+
