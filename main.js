714a186eafcd689ba21c29f9254980281a05c717
Masadavide <davide.masadm@gmail.com>
2021-12-10 16:21:48 +0100

    finito ma non stampa in console.log

--- main.js
+++ main.js
@@ -3,13 +3,16 @@ const scatola = document.getElementById('container')
 //scrive da 1 a 100 in console.log e in "scatola (HTML)"
 
 for(let i = 1; i <= 100; i++){
-    console.log('iterazione: ', i);
-    scatola.innerHTML += '<div class="box">' + i + '</div>'
-    if( (i%3 == 0) && (i%5 == 0) && (i !=0) ){                    //milestone 1, multipli di 3 e 5 FizzBuzz, di 3 Fizz, di 5 Buzz
-        scatola.innerHTML += '<div class="box fizzbuzz_color">' + i + '</div>'
+    if( (i%15 == 0) ){                    //Multipli di 3 e 5 FizzBuzz, di 3 Fizz, di 5 Buzz
+        scatola.innerHTML += '<div class="box fizzbuzz_color">' + "FizzBuzz" + '</div>'
+    }else if( (i%3 == 0) ){
+        scatola.innerHTML += '<div class="box fizz_color">' + "Fizz" + '</div>'
+    }else if( (i%5 == 0) ){
+        scatola.innerHTML += '<div class="box buzz_color">' + "Buzz" + '</div>'
     }else{
-
+        scatola.innerHTML += '<div class="box">' + i + '</div>'
     }
+    console.log('iterazione: ', i);
 }
 
 
