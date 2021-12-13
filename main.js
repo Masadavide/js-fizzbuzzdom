d7a3544f1fd8bbe363917f46426efdd8a368185b
Masadavide <davide.masadm@gmail.com>
2021-12-13 11:28:25 +0100

    correzione

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ const scatola = document.getElementById('container')
 
 for(let i = 1; i <= 100; i++){
     if( (i%15 == 0) ){                    //Multipli di 3 e 5 FizzBuzz, di 3 Fizz, di 5 Buzz
-        scatola.innerHTML += '<div class="box fizzbuzz_color">' + "FizzBuzz" + '</div>'
+        scatola.innerHTML += '<div class="box fizzbuzz_color"> FizzBuzz </div>';
     }else if( (i%3 == 0) ){
-        scatola.innerHTML += '<div class="box fizz_color">' + "Fizz" + '</div>'
+        scatola.innerHTML += '<div class="box fizz_color"> Fizz </div>'
     }else if( (i%5 == 0) ){
-        scatola.innerHTML += '<div class="box buzz_color">' + "Buzz" + '</div>'
+        scatola.innerHTML += '<div class="box buzz_color"> Buzz </div>'
     }else{
         scatola.innerHTML += '<div class="box">' + i + '</div>'
     }
